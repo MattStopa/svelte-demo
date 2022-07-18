10a2b02c13be7d4a86a36685bd8db40173a9119a
Matthew Stopa <matthew.p.stopa@gmail.com>
2022-07-18 03:50:59 -0400

    Svelte is amazing!

--- src/pages/TwoSidedTasks.svelte
+++ src/pages/TwoSidedTasks.svelte
@@ -2,16 +2,20 @@
   import Card from "../Card.svelte"
   import TaskList from '../tasks/TaskList.svelte'
 
-  let tasks =["Work to do", "more work to do", "Other Stuff you don't want"]
-  let trashTasks =["Working for the weekend"]
+  let tasks =[
+    {title: "Work to do", value: 1},
+    {title: "Steady as she goes", value: 2},
+    {title: "Super hard task!", value: 3}
+  ]
+  let trashTasks =[{title: "Weekend at bernies", value: 3}]
 
-  function addTask(task) { 
-    tasks.push(task)
+  function addTask(task, value) { 
+    tasks.push({title: task, value: value})
     tasks = tasks
   }
 
   function completeTask(i) { 
-    let result = tasks.splice(i, 1)
+    let result = tasks.splice(i, 1)[0]
     trashTasks.push(result)
     trashTasks = trashTasks
     tasks = tasks
@@ -29,7 +33,7 @@
     <div class="w-1/2">
       <TaskList 
         tasks={tasks} 
-        addTask={(v) => addTask(v) } 
+        addTask={(task, value) => addTask(task, value) } 
         completeTask={(v) => completeTask(v)}
         deleteTask={(v) => deleteTask(v)}>
       </TaskList>
--- src/tasks/TaskList.svelte
+++ src/tasks/TaskList.svelte
@@ -1,20 +1,57 @@
 <script>
-  export let  tasks = []
+  export let selectedSize = 1;
+  export let tasks = []
   export let addTask = () => { }
   export let completeTask = () => { }
   export let deleteTask = () => { }
   export let completedMode = false
+
+  function getEmojii(val) {
+    switch(val) {
+      case 1:
+        return '👶';
+      case 2:
+        return '🧆';
+      case 3:
+        return '👷';
+    }
+  }
+
+  function getSideClass(val) {
+    switch(val) {
+      case 1:
+        return 'border-l-4 border-green-400';
+      case 2:
+        return 'border-l-4 border-yellow-500';
+      case 3:
+        return 'border-l-4 border-red-400';
+    }
+  }
+
 </script>
 
 <div class="bg-gray-100 p-3">
   <div class="flex w-full"> 
     {#if !completedMode} 
       <div class="bg-blue-100  w-full px-3 py-3 m-5 shadow-lg rounded">
-
+        <div class="flex mb-2">
+          <div class="hover:bg-blue-200 cursor-pointer p-2 bg-white rounded mr-1 font-semibold {selectedSize == 1 ? 'bg-blue-300' : ''}"
+            on:click={() => selectedSize = 1}>
+            Small 👶
+          </div>
+          <div class="hover:bg-blue-200 cursor-pointer  p-2 bg-white rounded mr-1  font-semibold {selectedSize == 2 ? 'bg-blue-300 text-white' : ''}"
+            on:click={() => selectedSize = 2}>
+            Medium 🧆
+          </div>
+          <div class="hover:bg-blue-200 cursor-pointer p-2 bg-white rounded mr-1  font-semibold {selectedSize == 3 ? 'bg-blue-300 text-white' : ''}"
+            on:click={() => selectedSize = 3}>
+            Large 👷
+          </div>
+        </div>
         <input 
           on:keypress={(e) => { 
             if(e.key === "Enter") {
-              addTask(e.target.value)
+              addTask(e.target.value, selectedSize)
               e.target.value = ''
             }
           }}
@@ -28,8 +65,11 @@
     {/if}
   </div>
   {#each tasks as val, i}
-    <div class=" bg-white px-3 py-2 rounded mb-1  hover:bg-green-400 hover:text-white  cursor-pointer flex justify-between">
-      {val}
+    <div class="{getSideClass(val.value)} bg-white px-3 py-2 rounded mb-1  hover:bg-green-400 hover:text-white  cursor-pointer flex justify-between">
+      <div class="flex">
+        <div class="mr-2">{getEmojii(val.value)}</div>
+        <div>{val.title}</div>
+      </div>
       {#if !completedMode}
         <div class="flex">
           <div 
